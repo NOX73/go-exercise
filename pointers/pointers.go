1365a9c8856b2387538fd2527657bedeeb5216da
Rozhnov Alexandr <nox73@ya.ru>
2013-02-07 00:48:09 +0400

    pointers

---
+++ pointers/pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  
+  var a string
+  fmt.Println("a=", a) //
+
+  a = "Mystring"
+
+  var b *string
+
+  fmt.Println("a=", a) //Mystring
+  fmt.Println("b=", b) //<nil>
+
+  b = &a
+
+  fmt.Println("b=", b)//0xf840028220
+  fmt.Println("*b=", *b)//Mystring
+}
+
